bf876473f9fd93dc6114ccfd43ec5d444e89121a
Vũ Hiếu <vuhieu@MacBook-Pro-cua-Hieu.local>
2021-04-01 15:44:54 +0700

    STAR

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics.Contracts;
+using System.Runtime.Serialization;
+using System.Globalization;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject sphere;
+    private Vector3 vector3;
+    public GameObject carCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //vector3 = new Vector3(0, 0.5f, -5.09f);
+        vector3 =  transform.position - sphere.transform.position;
+        carCam = GameObject.Find("carCam");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {    // kết nối với tranform của component của vật thể Camera
+        transform.position = Vector3.Lerp(this.transform.position, carCam.transform.position, 50 * Time.deltaTime);
+        this.transform.LookAt(sphere.transform);
+        
+    }
+
+}
---
+++ Assets/Scripts/RepeatGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RepeatGame : MonoBehaviour
+{
+    public void EndGame()
+    {
+        StartCoroutine("GameOver");
+    }
+
+    private IEnumerable GameOver()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
